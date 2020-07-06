d1d4c9e6e83f2f6cb5a50ee1ee10cde4b9f02f10
YushiOMOTE <yushiomote@gmail.com>
2020-07-06 19:55:06 +0900

    Update generation script

--- gen/gen.sh
+++ gen/gen.sh
@@ -1,27 +1,51 @@
-#!/bin/sh
+#!/bin/bash
 
 set -uex
 
-spec=mattermost-openapi-v4.yaml
-ext=../ext/ref
-
 pushd "$(dirname "${BASH_SOURCE[0]}")"
+
+spec=mattermost-openapi-v4.yaml
+spec_path=$(pwd)/$spec
+ext=$(pwd)/../ext/ref
+name=mmcli_raw
 
 # Generate spec
 pushd $ext
-make build
+spec_src=v4/source
+cat $spec_src/introduction.yaml > $spec_path
+cat $spec_src/users.yaml >> $spec_path
+cat $spec_src/status.yaml >> $spec_path
+cat $spec_src/teams.yaml >> $spec_path
+cat $spec_src/channels.yaml >> $spec_path
+cat $spec_src/posts.yaml >> $spec_path
+cat $spec_src/preferences.yaml >> $spec_path
+cat $spec_src/jobs.yaml >> $spec_path
+cat $spec_src/system.yaml >> $spec_path
+cat $spec_src/emoji.yaml >> $spec_path
+cat $spec_src/webhooks.yaml >> $spec_path
+cat $spec_src/saml.yaml >> $spec_path
+cat $spec_src/compliance.yaml >> $spec_path
+cat $spec_src/cluster.yaml >> $spec_path
+cat $spec_src/brand.yaml >> $spec_path
+cat $spec_src/commands.yaml >> $spec_path
+cat $spec_src/oauth.yaml >> $spec_path
+cat $spec_src/elasticsearch.yaml >> $spec_path
+cat $spec_src/bleve.yaml >> $spec_path
+cat $spec_src/dataretention.yaml >> $spec_path
+cat $spec_src/roles.yaml >> $spec_path
+cat $spec_src/schemes.yaml >> $spec_path
+cat $spec_src/service_terms.yaml >> $spec_path
+cat $spec_src/opengraph.yaml >> $spec_path
+cat $spec_src/reactions.yaml >> $spec_path
+cat $spec_src/actions.yaml >> $spec_path
+cat $spec_src/bots.yaml >> $spec_path
+cat $spec_src/definitions.yaml >> $spec_path
 popd
 
 # Generate client
-cp $ext/v4/html/static/$spec .
 rm -rf out
-docker run --rm -v $(pwd):/local openapitools/openapi-generator-cli generate -i /local/$spec -g rust -c /local/config.json -o /local/out
+docker run --rm -v $(pwd):/local -u $(id -u) openapitools/openapi-generator-cli generate -i /local/$spec -g rust -c /local/config.json -o /local/out
 
-# Check if build passes
-pushd out
-cargo build
-popd
-
-# Update mmcli_raw
-rm -rf ../mmcli_raw
-mv out ../mmcli_raw
+# Update output crate
+rm -rf ../$name
+mv out ../$name
